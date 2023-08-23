897a763466e11d2580a2338162202a24119cc6b8
Jairvsosa98 <jairvillazonsosa98@outlook.com>
2023-08-22 19:10:05 -0500

    update environments x3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:9090/inventory-app/v1'
+    baseUrl: 'inventories-backend-sb-production.up.railway.app/inventory-app/v1'
 };
